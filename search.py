c53348a78ed354ea797939c92f37e850c2a97098
Vinicius <floresvinicius1605@gmail.com>
2025-01-25 09:27:06 -0300

    Adição do loop while em Main, retirando a recursão.

--- search.py
+++ search.py
@@ -4,27 +4,31 @@ def main():
     # conectar com servidor sqlite
     con = sqlite3.connect("books.db")
     cur = con.cursor()
-
-    print('Hello, welcome to our library! How do you want to perform a search? ')
-
+    
     while True:
-        # Selecionar tipo de busca
-        n = int(input('Enter 1 to search by book title, 2 to search by author: '))
-        if n == 1 or n == 2:
+        print('Hello, welcome to our library! How do you want to perform a search? ')
+
+        while True:
+            # Selecionar tipo de busca
+            n = int(input('Enter 1 to search by book title, 2 to search by author: '))
+            if n == 1 or n == 2:
+                break
+
+        if n == 1:
+            title = input('Enter the title of the book: ')
+            out = title_search(title, cur)
+
+        else:
+            name = input('Enter the name of the author: ')
+
+        if out == False:
             break
-
-    if n == 1:
-        title = input('Enter the title of the book: ')
-        title_search(title, cur)
-
-    else:
-        name = input('Enter the name of the author: ')
 
 
 def title_search(t, cur):
     # Verificar se o nome do livro está no banco de dados
     cur.execute("SELECT title FROM book WHERE title = ?", (t,))
-    found = cur.fetchone()
+    found = cur.fetchone()[0]
 
     # Caso não seja encontrado
     if found == None:
@@ -36,15 +40,14 @@ def title_search(t, cur):
             number = int(input('Enter 1 to perform a new search, 2 to close: '))
             if number == 1 or number == 2:
                 break
-
-        if number == 1:
-            main() # Se ele quiser, chamar main novamente, para que a pesquisa seja realizada de novo
+        if n == 1:
+            return True
         
         else:
             return False # Caso contrário, finalizar o programa
     else:
         cur.execute("SELECT author.name FROM author INNER JOIN book ON author.id = book.id_author WHERE book.title = ?", (t,) )
-        author_name = cur.fetchone() 
+        author_name = cur.fetchone()[0] 
         print(f'The book you are looking for is {t} by {author_name}? ') # Se houver um livro com o nome que o usuário digitou no banco de dados, verificar se é o mesmo livro, conferindo o autor
 
         while True:
@@ -61,7 +64,7 @@ def title_search(t, cur):
                 if n == 1 or n == 2:
                     break # Caso contrário, realizar uma nova busca ou fechar o programa
             if n == 1:
-                main()
+                return True
             
             else:
                 return False
