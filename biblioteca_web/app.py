508eafb1391110d1adee0155d8da796e0290115a
Vinicius <floresvinicius1605@gmail.com>
2025-03-01 10:01:43 -0300

    Integração com o banco de dados

--- biblioteca_web/app.py
+++ biblioteca_web/app.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+def database_connection():
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = sqlite3.Row #habilitar pesquisa por nomes de colunas
+    return conn
