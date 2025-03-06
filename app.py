1a5b615fcc86efbf046f6ed557cf177e1d3d1150
Vinicius <floresvinicius1605@gmail.com>
2025-03-06 18:36:40 -0300

    Exclusão de search_type

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    search_type = request.form.get('search_type')
     query = request.form.get('query').strip().title()
 
     try:
@@ -30,18 +29,22 @@ def search():
 
         cur.execute("SELECT id FROM author WHERE name LIKE ?", ('%' + query + '%',))
         authors = cur.fetchall()
-
-        author_ids = [author['id'] for author in authors]
-        placeholders = ','.join(['?'] * len(author_ids))
-        cur.execute(f"SELECT title, borrowed FROM book WHERE id_author IN ({placeholders})", author_ids)
-
-        results = cur.fetchall()
-
-        for book in results:
-            books.append(book)
         
-        conn.close()
+        if authors:
+            author_ids = [author['id'] for author in authors]
+            placeholders = ','.join(['?'] * len(author_ids))
+
+            cur.execute(f"SELECT title, borrowed FROM book WHERE id_author IN ({placeholders})", author_ids)
+
+            results = cur.fetchall()
+
+            for book in results:
+                books.append(book)
+        
         return render_template("search.html", query=query, books=books)
 
     except:
         return("error.html")
+    
+    finally:
+        conn.close()
